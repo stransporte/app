9689cdd453c46d4ebc07d047ded8bea31d9c9e8b
rickypazd <rickypazd@hotmail.com>
2023-10-14 08:18:36 -0400

    Update ssh_setup.sh

--- .github/actions/ssh_setup.sh
+++ .github/actions/ssh_setup.sh
@@ -4,13 +4,33 @@
 mkdir -p ~/.ssh
 echo "creo la carpeta"
 
-
 cp ./public/id_rsa ~/.ssh/id_rsa
 echo "copio la llave"
 
-
 chmod 600 ~/.ssh/id_rsa
 echo "Dio permisos"
+
+#!/bin/bash
+
+# Archivo de configuración SSH
+config_file="~/.ssh/config"
+
+# Comprobar si el archivo de configuración ya existe
+if [ ! -f "$config_file" ]; then
+    touch "$config_file"
+fi
+
+# Verificar si ya existe una entrada para el host
+if grep -qFx "Host $SSH_URL" "$config_file"; then
+    echo "La entrada para el host ya existe en el archivo de configuración."
+else
+    # Agregar la entrada del host al archivo de configuración
+    echo "Host $SSH_URL" >>"$config_file"
+    echo "  HostName $SSH_URL" >>"$config_file"
+    echo "  User $SSH_USER" >>"$config_file"
+    echo "  Port $SSH_PORT" >>"$config_file"
+    echo "La entrada para el host se ha agregado al archivo de configuración."
+fi
 
 ssh-keyscan -t rsa $SSH_URL >>~/.ssh/known_hosts
 echo "Termino el scan"
