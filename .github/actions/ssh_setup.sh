1733bb4874883d9992fec322da54408347c5f7ef
rickypazd <rickypazd@hotmail.com>
2023-10-14 10:19:28 -0400

    Update ssh_setup.sh

--- .github/actions/ssh_setup.sh
+++ .github/actions/ssh_setup.sh
@@ -8,5 +8,7 @@ chmod 600 ~/.ssh/id_rsa
 ssh_config_file=".ssh/config"
 host_name="$SSH_SERVER_HOST"
 hostname=$(grep -A2 "Host $host_name" $ssh_config_file | awk '/HostName/{print $2}')
-echo "HostName para el Host $host_name es: $hostname"
-ssh-keyscan -t rsa $hostname >>~/.ssh/known_hosts
+port=$(grep -A2 "Host $host_name" $ssh_config_file | awk '/Port/{print $2}')
+echo "HostName para el Host $host_name es: $hostname:$port"
+
+ssh-keyscan -t rsa -p $port $hostname >>~/.ssh/known_hosts
