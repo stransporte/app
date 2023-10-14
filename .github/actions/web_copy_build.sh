0e5faeed923dd5cb1cae39c43f40b8f1ee105bb9
rickypazd <rickypazd@hotmail.com>
2023-10-14 09:15:32 -0400

    Update web_copy_build.sh

--- .github/actions/web_copy_build.sh
+++ .github/actions/web_copy_build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 . $GITHUB_WORKSPACE/.env
 
-scp "$GITHUB_WORKSPACE/README.md" "$SSH_SERVER_HOST:/home/servisofts/README.md"
-# scp -r $GITHUB_WORKSPACE/build "$SSH_SERVER_HOST":/home/servisofts/servicios/stransporte/entornos/stransporte/
+scp ./README.md "$SSH_SERVER_HOST:/home/servisofts/README.md"
