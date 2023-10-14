b719f2a4477d85a5637cca9b3b5e13713d350f15
rickypazd <rickypazd@hotmail.com>
2023-10-14 09:51:37 -0400

    Update ssh_setup.sh

--- .github/actions/ssh_setup.sh
+++ .github/actions/ssh_setup.sh
@@ -2,9 +2,8 @@
 . $GITHUB_WORKSPACE/.env
 
 mkdir -p ~/.ssh
-cp -r ./.ssh ~/.ssh/
+cp -r $GITHUB_WORKSPACE/.ssh ~/
 
 chmod 600 ~/.ssh/id_rsa
 
-ssh-keyscan -t rsa $SSH_SERVER_HOST >> ~/.ssh/known_hosts
-
+ssh-keyscan -t rsa $SSH_SERVER_HOST >>~/.ssh/known_hosts
