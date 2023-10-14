8d1dfdf661f3f36b709d1954b0cead8f22a26062
rickypazd <rickypazd@hotmail.com>
2023-10-14 09:09:15 -0400

    asd

--- .github/actions/web_copy_build.sh
+++ .github/actions/web_copy_build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 . $GITHUB_WORKSPACE/.env
 
-scp -r $GITHUB_WORKSPACE/build $SSH_HOST:/home/servisofts/servicios/stransporte/entornos/stransporte/
+scp -r $GITHUB_WORKSPACE/README.md "$SSH_SERVER_HOST":~/
+# scp -r $GITHUB_WORKSPACE/build "$SSH_SERVER_HOST":/home/servisofts/servicios/stransporte/entornos/stransporte/
