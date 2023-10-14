8a69a3cedda64ba8cac019cf434806d2485f9f39
rickypazd <rickypazd@hotmail.com>
2023-10-14 07:45:15 -0400

    Update ssh_setup.sh

--- .github/actions/ssh_setup.sh
+++ .github/actions/ssh_setup.sh
@@ -2,8 +2,16 @@
 . $GITHUB_WORKSPACE/.env
 
 mkdir -p ~/.ssh
+echo "creo la carpeta"
+
 cp ./public/id_rsa ~/.ssh/id_rsa
+echo "copio la llave"
+
+
 chmod 600 ~/.ssh/id_rsa
+echo "Dio permisos"
+
 ssh-keyscan -t rsa SSH_URL >>~/.ssh/known_hosts
+echo "Termino el scan"
 
 scp -r ./README.md $SSH_USER@$SSH_URL:~/
