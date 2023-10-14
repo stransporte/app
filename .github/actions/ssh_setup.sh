5662a350182d580c2f3699efc55599c89ab14d4e
rickypazd <rickypazd@hotmail.com>
2023-10-14 09:41:06 -0400

    Update ssh_setup.sh

--- .github/actions/ssh_setup.sh
+++ .github/actions/ssh_setup.sh
@@ -6,7 +6,7 @@ cp -r ./.ssh ~/.ssh/id_rsa
 
 chmod 600 ~/.ssh/id_rsa
 
-ssh-keyscan -t rsa "$SSH_SERVER_HOST" >>~/.ssh/known_hosts
+ssh-keyscan -t rsa servisofts.com >>~/.ssh/known_hosts
 
 echo "Termino el scan"
 
